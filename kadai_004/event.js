2cfbe8cee5c9e08d8aeac9c2ac335fbcc2b46ea7
t-tak-1090 <130780527+t-tak-1090@users.noreply.github.com>
2025-02-07 07:46:52 +0900

    Update event.js

--- kadai_004/event.js
+++ kadai_004/event.js
@@ -2,6 +2,6 @@ $(window).on('load',function(){
     console.log('loadイベントが発生しました');
 });
 
-$(document).on('scroll',function(){
+$(window).on('scroll',function(){
     console.log('scrollイベントが発生しました');
 });
